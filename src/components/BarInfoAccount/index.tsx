639e91a4045b8b7c10e77ff00a592ad79143fed6
willmorim <pdbawilliam@gmail.com>
2020-12-22 15:53:59 -0300

    BarInfoAccount finalizada

--- src/components/BarInfoAccount/index.tsx
+++ src/components/BarInfoAccount/index.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
 
+import {
+  Bar,
+  Container,
+  InfoBar,
+  Tweets,
+  Followings,
+  Followers,
+  Favorites,
+  Lists,
+  Moments,
+  Actions,
+  Button,
+} from './styles';
+
+import moreImg from '../../assets/more.svg';
+
 const BarInfo: React.FC = () => {
-  return <h1>BarInfo</h1>;
+  return (
+    <Bar>
+      <Container>
+        <InfoBar>
+          <Tweets>
+            <span>Tweets</span>
+            <strong>3931</strong>
+          </Tweets>
+          <Followings>
+            <span>Followings</span>
+            <strong>654</strong>
+          </Followings>
+          <Followers>
+            <span>Followers</span>
+            <strong>387</strong>
+          </Followers>
+          <Favorites>
+            <span>Favorites</span>
+            <strong>265</strong>
+          </Favorites>
+          <Lists>
+            <span>Lists</span>
+            <strong>8</strong>
+          </Lists>
+          <Moments>
+            <span>Moments</span>
+            <strong>0</strong>
+          </Moments>
+        </InfoBar>
+
+        <Actions>
+          <Button>Follow</Button>
+          <img src={moreImg} alt="More" />
+        </Actions>
+      </Container>
+    </Bar>
+  );
 };
 
 export default BarInfo;
--- src/components/BarInfoAccount/styles.ts
+++ src/components/BarInfoAccount/styles.ts
@@ -1,3 +1,179 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Bar = styled.div`
+  height: 59px;
+  background: #fff;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25);
+`;
+
+export const Container = styled.div`
+  max-width: 1170px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+  padding: 0 30px;
+  padding-left: 285px;
+`;
+
+export const InfoBar = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+  height: 100%;
+`;
+
+export const Tweets = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 15px;
+  position: relative;
+  height: 100%;
+
+  ::after {
+    content: '';
+    width: 100%;
+    height: 2px;
+    background: #3bb9e3;
+    left: 0;
+    bottom: 0;
+    position: absolute;
+  }
+
+  span {
+    color: #667580;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #667580;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 2px;
+    color: #3bb9e3;
+  }
+`;
+
+export const Followings = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+
+  span {
+    color: #667580;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #667580;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 2px;
+  }
+`;
+
+export const Followers = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+
+  span {
+    color: #667580;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #667580;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 2px;
+  }
+`;
+
+export const Favorites = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+
+  span {
+    color: #667580;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #667580;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 2px;
+  }
+`;
+
+export const Lists = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+
+  span {
+    color: #667580;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #667580;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 2px;
+  }
+`;
+
+export const Moments = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+
+  span {
+    color: #667580;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  strong {
+    color: #667580;
+    font-size: 18px;
+    font-weight: bold;
+    margin-top: 2px;
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+`;
+
+export const Button = styled.button`
+  height: 34px;
+
+  align-items: center;
+  justify-content: center;
+
+  width: 90px;
+  font-weight: bold;
+  font-size: 14px;
+  color: #3bb9e3;
+  border: 1px solid #3bb9e3;
+  border-radius: 16px;
+
+  margin-right: 20px;
+`;
